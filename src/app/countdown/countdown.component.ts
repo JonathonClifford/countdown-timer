2c30c5c900a8e4c62633fc478af1446d4bb6b8ac
Jonathon Clifford <jonathon.clifford@switchdin.com>
2021-01-10 23:29:17 +1100

    Included the new countdown component in the main html

--- src/app/countdown/countdown.component.ts
+++ src/app/countdown/countdown.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CountdownComponent implements OnInit {
 
+  startCount = 5;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  subtract(): void {
+    for (let i = 0; i < 5; i++) {
+      window.setTimeout(() => console.log(this.startCount), 1000);
+      this.startCount--;
+      console.log('function ran');
+      console.log(this.startCount);
+    }
+  }
+
+  countUpFunc(): void {
+    const task = async () => {
+      for (let i = 0; i < 5; i++) {
+        await new Promise(r => setTimeout(r, 1000));
+        this.startCount--;
+        console.log(this.startCount);
+      }
+    };
+    task();
   }
 
 }
