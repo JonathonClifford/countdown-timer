d9b9c6da8c8b33176f9ae93d4f972b123f55f883
Jonathon Clifford <jonathon.clifford@switchdin.com>
2021-01-29 01:09:55 +1100

    Time countdown component

---
+++ src/app/time-countdown/time-countdown.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TimeCountdownComponent } from './time-countdown.component';
+
+describe('TimeCountdownComponent', () => {
+  let component: TimeCountdownComponent;
+  let fixture: ComponentFixture<TimeCountdownComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TimeCountdownComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TimeCountdownComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/time-countdown/time-countdown.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-time-countdown',
+  templateUrl: './time-countdown.component.html',
+  styleUrls: ['./time-countdown.component.scss']
+})
+export class TimeCountdownComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
