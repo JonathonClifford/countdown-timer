2b50475125a56b80bc35c5715f2d87d8ddcda55e
Jonathon Clifford <jonathon.clifford@switchdin.com>
2021-01-29 03:04:33 +1100

    Timer with basic countdown formatting e.g. 00:00:00

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { TimerPipePipe } from './timer-pipe.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CountdownComponent,
-    TimeCountdownComponent
+    TimeCountdownComponent,
+    TimerPipePipe
   ],
   imports: [
     BrowserModule,
--- src/app/countdown/countdown.component.ts
+++ src/app/countdown/countdown.component.ts
@@ -9,6 +9,7 @@ export class CountdownComponent implements OnInit {
 
   startCount = 5;
   inputCountdown: number = 0;
+  inputCountdownTimerFormat: string = "00:00:00";
 
   constructor() { }
 
@@ -62,16 +63,37 @@ export class CountdownComponent implements OnInit {
   }
 
   updateWithHours(hours: string, minutes: string, seconds: string) {
+    var hoursNum: number = parseInt(hours);
+    var minutesNum: number = parseInt(minutes);
+    var secondsNum: number = parseInt(seconds);
     this.inputCountdown = parseInt(hours) * 3600 + parseInt(minutes) * 60 + parseInt(seconds);
     var count:number = this.inputCountdown;
     const task = async () => {
       for (let i = 0; i < count; i++) {
         await new Promise(r => setTimeout(r, 1000));
         this.inputCountdown--;
+        this.inputCountdownTimerFormat = this.formatTimerString(this.inputCountdown);
         console.log(this.inputCountdown);
       }
     };
     task();
+  }
+
+  formatTimerString(seconds: number): string {
+    let numHours: number = Math.floor(seconds / 3600);
+    let numMinutes: number = Math.floor((seconds - (numHours * 3600)) / 60);
+    let numSeconds: number = Math.floor(seconds - (numHours * 3600 + numMinutes * 60));
+    return this.formatDoubleCharacter(numHours) + ":" +
+      this.formatDoubleCharacter(numMinutes) + ":" +
+      this.formatDoubleCharacter(numSeconds);
+  }
+
+  formatDoubleCharacter(value: number): string {
+    if (value < 10) {
+      return "0" + value.toString();
+    } else {
+      return value.toString();
+    }
   }
 
 }
